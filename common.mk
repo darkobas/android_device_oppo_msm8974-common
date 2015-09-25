ed9eb374a37146f676ea2938ecf903f38c7f92f1
darkobas <darkobas@gmail.com>
2015-09-25 11:12:44 +0200

    Revert "msm8974: Enable fluence for audio record usecase"
    This reverts commit 515e2905e2c116b3d7adcaafd77b812672403eef.

--- common.mk
+++ common.mk
@@ -254,7 +254,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     use.voice.path.for.pcm.voip=true \
     ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.audiorec=true \
     av.offload.enable=true \
     av.streaming.offload.enable=false \
     audio.offload.pcm.enable=true \
